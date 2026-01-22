19931b1d01b38023868e31bf6a847b3a6bba1c88
Thamindu Kavinda <dinujayatk@gmail.com>
2026-01-22 11:11:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "Hello from Flask on ECS Fargate!\n"
+
+@app.get("/health")
+def health():
+    return jsonify(status="ok"), 200
+
+if __name__ == "__main__":
+    # Important: listen on 0.0.0.0 and port 8080
+    app.run(host="0.0.0.0", port=8080)
